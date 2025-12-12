4a2ac4339041ea0c21f157276db6c9ab49c64582
silent-moment <first_nothin@yahoo.com>
2025-12-12 20:42:09 +0300

    Delete pages directory

--- pages/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40, fontFamily: "system-ui" }}>
-      <h1>Petrotech-Peinso v2</h1>
-      <p>It works ✅</p>
-    </main>
-  )
-}
