6de1ffef583ff4fa5f5b4cf8227b475658ead342
silent-moment <first_nothin@yahoo.com>
2025-12-12 20:48:34 +0300

    Create index.tsx

---
+++ ✅ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>Petrotech-Peinso v2</h1>
+      <p>Home page is working ✅</p>
+    </main>
+  )
+}
+  
